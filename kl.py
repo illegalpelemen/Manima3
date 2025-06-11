8439392c6f7491faa3fbbbb7b4f808697ba84e3b
jeck-vorobeq <m.a.karasev2011@gmale.com>
2025-06-11 17:06:32 +0300

    ..----.-...--.-.-

--- kl.py
+++ kl.py
@@ -1,7 +1,8 @@
 print("hello world")
 from manim import *
-
-class SquareToCircle(Scene):
+config.pixel_width = 1920
+config.pixel_height = 1080
+class SquareToCircle(ThreeDScene):
     def construct(self):
         # circle = Circle(color = BLUE ,fill_color=RED ,fill_opacity=0.1)
         # circleeanim = Create(circle)
@@ -15,14 +16,19 @@ class SquareToCircle(Scene):
         # fadeout = FadeOut(circle)
         #
         # self.play(fadeout)
-        circle = Circle(color=YELLOW).shift(LEFT * 2)
-        self.add(circle)
+        #circle = Circle(color=YELLOW).shift(LEFT * 2)
+        #self.add(circle)
+        square = Square(fill_color= YELLOW,fill_opacity=1)
+        square1 = Square(fill_color= RED,fill_opacity=1)
+        axes = Axes()
+        self.add(axes)
+        self.add(square)
+        self.add(square1)
+        square1.move_to([0,3,6])
+        #self.play(square.animate.move_to(ORIGIN))
+        self.wait(3)
+        #self.set_camera_orientation(theta= 45)
+        self.camera.set_focal_distance(1)
+        self.move_camera(theta = 360 * DEGREES,run_time = 6)
+        self.wait(3)
 
-        self.play(circle.animate.move_to(ORIGIN))
-        self.wait()
-
-        self.play(circle.animate.shift(UP * 2))
-        self.wait()
-
-        self.play(circle.animate.to_edge(DOWN))
-        self.wait()
