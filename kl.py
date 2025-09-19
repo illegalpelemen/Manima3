77c5be40ae1e062ea186109889ac5d84bb2cebc7
KM <lalalabububu@gmail.com>
2025-09-19 17:11:37 +0300

    --------

--- kl.py
+++ kl.py
@@ -1,6 +1,8 @@
 
 from manim import *
-
+def graphic(x):
+    print(x,x**x)
+    return float(x**x)
 class GraphCreate(Scene):
     def construct(self):
         plane = NumberPlane(
@@ -8,7 +10,7 @@ class GraphCreate(Scene):
             y_range=[-2, 4, 1],
             axis_config={"include_numbers": True}
         )
-        graph = plane.plot(lambda x: x**2, color=RED)
+        graph = plane.plot(graphic, color=RED)
 
         self.add(plane)
         self.play(Create(graph), run_time=3)  # график рисуется за 3 секунды
