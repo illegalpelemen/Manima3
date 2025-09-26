97a6b7bbe62ecd7529c96d015637f04b3c727662
KM <lalalabububu@gmail.com>
2025-09-26 17:05:44 +0300

    --------

--- kl.py
+++ kl.py
@@ -1,18 +1,29 @@
-
+import random
 from manim import *
+
+x1 = 0
+
+
 def graphic(x):
-    print(x,x**x)
-    return float(x**x)
+    global x1
+    rand = random.choice([0.1, -0.1])
+    x1 = x1 + rand
+    return float(x1)
+
+
 class GraphCreate(Scene):
     def construct(self):
         plane = NumberPlane(
             x_range=[-3, 3, 1],
-            y_range=[-2, 4, 1],
+            y_range=[-4, 4, 1],
             axis_config={"include_numbers": True}
         )
         graph = plane.plot(graphic, color=RED)
-
-        self.add(plane)
+        coords = graphic(0)
+        print(coords)
+        dot = Dot([0, coords, 0], color=BLUE)
+        text = Text(str(coords))
+        self.add(plane, dot)
+        self.add(text.next_to(dot, UP, buff=SMALL_BUFF))
         self.play(Create(graph), run_time=3)  # график рисуется за 3 секунды
         self.wait()
-
