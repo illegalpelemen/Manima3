5e3f2167a24d682bedc5ac9031f8a9a50508f28a
KM <lalalabububu@gmail.com>
2025-10-03 17:02:57 +0300

    --...----...-..---...--..

--- kl.py
+++ kl.py
@@ -1,29 +1,37 @@
 import random
 from manim import *
 
-x1 = 0
-
+y = 0.3
+id_x = None
 
 def graphic(x):
-    global x1
+    global y, id_x
+
     rand = random.choice([0.1, -0.1])
-    x1 = x1 + rand
-    return float(x1)
+    y = y + rand
+    print(y)
+    if round(y,2) == 0:
+        id_x = x
+
+    return float(y)
 
 
 class GraphCreate(Scene):
     def construct(self):
+        global y
         plane = NumberPlane(
             x_range=[-3, 3, 1],
             y_range=[-4, 4, 1],
             axis_config={"include_numbers": True}
         )
         graph = plane.plot(graphic, color=RED)
-        coords = graphic(0)
-        print(coords)
-        dot = Dot([0, coords, 0], color=BLUE)
-        text = Text(str(coords))
+
+        # print(coords)
+        dot = Dot([0, 3, 0], color=BLUE)
+        text = Text(str(round(y, 2)), color=BLUE)
         self.add(plane, dot)
-        self.add(text.next_to(dot, UP, buff=SMALL_BUFF))
+        if id_x is not None:
+            self.add(Dot([id_x,0,0],color=BLUE))
+        self.add(text.next_to(dot, UP))
         self.play(Create(graph), run_time=3)  # график рисуется за 3 секунды
         self.wait()
