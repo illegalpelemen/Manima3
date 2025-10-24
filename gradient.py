9e2dd7ce995df54a2506ab5e40d05bbe1a5e9407
KM <lalalabububu@gmail.com>
2025-10-24 17:10:36 +0300

    --...----...-/-..---..--.-..--.---.....--.-...-/

--- gradient.py
+++ gradient.py
@@ -1,23 +1,31 @@
 from manim import *
 import random
 class Gradient(Scene):
-    def construct(self):
-        svx = 3
-        svy = 3
-        a= []
+    def gen_list(self,svx,svy):
 
-
+        a = []
+        hh = 255
         for q in range(svy):
             b = []
             for i in range(svx):
-                b.append(i)
+                if q == 0 or q == svy-1 or i == 0 or i == svx-1 or q == i or q + i == svy-1:
+                    b.append(hh)
+                else:
+                    b.append(i)
+
             a.append(b)
         for r in a:
-            print(r)
+            for x in r:
+                print(str(x).zfill(3),end =" ")
+            print()
+        return a
+    def construct(self):
+
+        a = self.gen_list(50,50)
 
 
         imageArray = np.uint8(a)
-        image = ImageMobject(imageArray).scale(2)
+        image = ImageMobject(imageArray).scale(20)
         image.background_rectangle = SurroundingRectangle(image, color=GREEN)
         self.add(image)
     #     self.image.add_updater(self.newimage)
