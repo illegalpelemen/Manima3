ce6568e8fbadbcbdecb7100bc87b243ddd65a144
KM <lalalabububu@gmail.com>
2025-11-07 17:08:06 +0300

    ...-/-..---..--.-..--.---.....--.-...-/-...---

---
+++ checkerboard.py
@@ -0,0 +1,26 @@
+from manim import *
+import random
+class Check(Scene):
+
+    def gen_list(self):
+        svx = 8
+        svy = 8
+        a = []
+
+        for q in range(svy):
+            b = []
+
+            for i in range(svx):
+                b.append(0)
+
+
+            a.append(b)
+        return a
+    def construct(self):
+        a = self.gen_list()
+        c = 5
+        r = range(0,c, 1)
+        for e in r:
+
+            self.add(Square(color=BLUE).move_to([e+e-6,0,0]))
+            print(e)
