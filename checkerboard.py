e8e7aed1404ea0a3293c9185a4fa6a2f66d02839
KM <lalalabububu@gmail.com>
2025-11-21 17:08:50 +0300

    ...-/-..---..--.-..--.---.....--.-...-/-...---

--- checkerboard.py
+++ checkerboard.py
@@ -1,5 +1,7 @@
 from manim import *
 import random
+
+
 class Check(Scene):
 
     def gen_list(self):
@@ -13,14 +15,20 @@ class Check(Scene):
             for i in range(svx):
                 b.append(0)
 
-
             a.append(b)
         return a
+
     def construct(self):
         a = self.gen_list()
-        c = 7
-        r = range(0,c, 1)
+        amount_of_squares = len(a[0])
+        amount_of_squaresy = len(a)
+        r = range(0, amount_of_squares, 1)
         for e in r:
+            self.add(self.square(amount_of_squares, amount_of_squaresy, e,1))
 
-            self.add(Square(color=BLUE).move_to([e+e-6,0,0]))
-            print(e)
+    def square(self, amount_of_squaresx, amount_of_squaresy, which_squarex,which_squarey):
+        scale = 14 / amount_of_squaresx
+        scaley = 6 / amount_of_squaresy
+        lighted_square = (Square(color=BLUE, side_length=scale)
+                          .move_to([which_squarex * scale - 7 + 0.5 * scale, which_squarey * scaley - 0.5* scaley , 0]))
+        return lighted_square
