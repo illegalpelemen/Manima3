4f10d8bc8765bcabaa341433412634c7d7260504
KM <lalalabububu@gmail.com>
2026-03-06 17:07:29 +0300

    --.-.-/.-.--./..--/.-/.-

--- lines_circles.py
+++ lines_circles.py
@@ -1,12 +1,26 @@
 from manim import *
 class Lines(Scene):
     def construct(self):
-        line1 = Line(LEFT,RIGHT)
-        dot = Dot(radius=0.2,color=WHITE)
-        self.add(line1)
-        for q in range(1,5):
-            self.play(MoveAlongPath(dot,line1))
-            self.wait(1)
-            self.play(dot.animate.move_to(line1.start))
-            self.wait(1)
-            self.play(Rotate(line1,45))
+        self.line1 = Line(LEFT,RIGHT)
+        self.dot1 = Dot(radius=0.2,color=WHITE)
+        self.gr1  = Group(self.line1,self.dot1)
+        self.x = 0
+        self.add(self.gr1)
+
+        self.gr1.add_updater(self.rot)
+        self.dot1.add_updater(self.move)
+        #self.play(MoveAlongPath(dot1, self.line1))
+        #self.play(MoveAlongPath(dot1, self.line1.reverse_direction()))
+        self.wait(10)
+    def rot(self,mob,dt):
+        mob.rotate(DEGREES * 5)
+    def move(self,mob,dt):
+
+        self.x += 0.01
+        if self.x > 1:
+            self.x = 1
+        point = self.line1.point_from_proportion(self.x)
+        mob.move_to(point)
+
+
+
